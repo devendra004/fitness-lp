902db64cb34023ce52448dd0037efdc4c87f3a63
jparihar69@gmail.com <jparihar69@gmail.com>
2017-04-19 14:44:47 +0530

    landing page v1

---
+++ index.php
@@ -0,0 +1,290 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+       <!-- set the encoding of your site -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Itonefitness</title>
+        <!-- include the site stylesheet -->
+        <link rel="stylesheet" type="text/css" href="css/master.css">
+
+</head>
+<script type="text/javascript">
+function downloadJSAtOnload() {
+var element = document.createElement("script");
+element.src = "js/jquery.js";
+document.body.appendChild(element);
+}
+if (window.addEventListener)
+window.addEventListener("load", downloadJSAtOnload, false);
+else if (window.attachEvent)
+window.attachEvent("onload", downloadJSAtOnload);
+else window.onload = downloadJSAtOnload;
+</script>
+<script type="text/javascript">
+function downloadJSAtOnload() {
+var element = document.createElement("script");
+element.src = "js/bootstrap.js";
+document.body.appendChild(element);
+}
+if (window.addEventListener)
+window.addEventListener("load", downloadJSAtOnload, false);
+else if (window.attachEvent)
+window.attachEvent("onload", downloadJSAtOnload);
+else window.onload = downloadJSAtOnload;
+</script>
+<body>
+            <!-- main container of all the page elements -->
+<header>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="pull-left">
+          <img class="logo-img" src="images/logo-img.png" alt="" width="200"/>
+        </div>
+        <div class="pull-right contact">
+          <small>Call Us For More Information</small>
+          <br>
+          <h3 class="head">0751 9234 207 &nbsp;&nbsp;<a href="https://www.facebook.com" target="_blank"><i class="fa fa-facebook font-icon"></i></a></h3>
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
+
+<div id="banner">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h1 class=" text-center heading-h1">Top Mobile Personal Trainers In London</h1>
+        <h2 class=" text-center heading-2"><span>Customised Diet Plan Included</span></h2>
+          <div class="col-md-7 col-lg-6">
+            <div class="novice">
+                <h3>Itone Fitness on your Doorsteps</h3>
+                <h4>Helping Novice Clients Achieve Professional Fitness Goals.</h4>
+            </div>
+            <div class="row db-padding-btm db-attached">
+              <div class="box">
+                <div class="red">
+                  <div class="price">
+                    <h1>30</h1>
+                    <small>1 Session</small>
+                  </div>
+                </div>
+                <div class="blue">
+                  <div class="price">
+                    <h1>120</h1>
+                    <small>5 Sessions + Diet Plan<br>Save ₤30</small>
+                  </div>
+                </div>
+                <div class="green">
+                  <div class="price">
+                    <h1>200</h1>
+                    <small>8 Sessions + Nutrition Weight + Profile + Diet Plan</small>
+                  </div>
+                </div>
+              </div>
+              <!-- <div class="col-md-4 col-sm-4">
+                <div class="db-wrapper">
+                  <div class="db-pricing-eleven db-bk-color-one">
+                    <div class="price">
+                        <sup>₤</sup>30
+                        <small>1 Session<br><br></small>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-4 col-sm-4">
+                <div class="db-wrapper">
+                  <div class="db-pricing-eleven db-bk-color-two popular">
+                    <div class="price">
+                      <sup>₤</sup>120
+                      <small>5 Sessions + Diet Plan<br>Save ₤30</small>
+                    </div>
+                  </div>
+                </div>
+              </div>
+               <div class="col-md-4 col-sm-4">
+                  <div class="db-wrapper">
+                    <div class="db-pricing-eleven db-bk-color-three">
+                      <div class="price">
+                          <sup>₤</sup>200
+                          <small>8 Sessions + Nutrition Weight + Profile + Diet Plan
+                          </small>
+                      </div>
+                    </div>
+                  </div>
+                </div> -->
+              </div>
+             <p class=" text-center wht wht-p">Book your free consultation with one<br> of London's top personal trainers</p>
+         </div>
+       <div class="col-md-5 col-lg-offset-1 contact-form">
+            <h1 class=" text-center banner-h1">Book Your
+            <span style="color:#fff;"> Free</span> Trial With 1 Top <br> Personal Trainer In London</h1>
+    <form method="post">
+          <div class="form-center">
+              <div class="from-group">
+                <label>Name</label>
+                 <input name="name" required="required" placeholder="Enter your Name" class="form-control" id="inputname" type="text"/>
+             </div>
+
+            <div class="from-group">
+              <label>Email Address</label>
+              <input name="email" required="required" placeholder="Enter your Email" class="form-control" id="inputemail" type="email"/>
+            </div>
+
+            <div class="from-group">
+              <label>Telephone</label>
+              <input name="telephone" required="required" placeholder="Enter Your Number" class="form-control" id="form-contant" type="text"/>
+            </div>
+
+            <div class="from-group">
+              <label>Comment</label>
+              <textarea name="comment" required="required" placeholder="" class="form-control" id="comment"></textarea>
+            </div>
+
+            <div class="from-group text-center">
+               <input class="btn btn-yellow" value="Book now" type="submit">
+            </div>
+         </div>
+
+            <div class="from-group">
+            <small>We will never share your personal information.<a class="menu_a" href="#">Privacy Policy</a></small>
+            </div>
+    </form>
+       </div>
+      </div>
+   </div>
+  </div>
+</div>
+    <div class="medium-contant">
+      <div class="container">
+         <div class="row">
+            <div class="col-md-12">
+              <h1 class="medium-contant-h1">Itone fitness helping you live better right on your door step</h1>
+            </div>
+          </div>
+      </div>
+    </div>
+<div id="testmoney">
+  <div class="container">
+    <div class="row">
+       <div class="col-md-12" data-wow-delay="0.2s">
+          <div class="carousel slide" data-ride="carousel" id="quote-carousel">
+                            <!-- Bottom Carousel Indicators -->
+                  <!-- Carousel Slides / Quotes -->
+                <div class="carousel-inner text-center">
+                              <!-- Quote 1 -->
+                     <div class="item">
+                        <blockquote>
+                          <div class="row">
+                            <div class="col-sm-8 col-sm-offset-2">
+                              <p>I started training with Detox-Fit around 3 months ago. My 'love handles' have disappeared almost completely and my fitness level has increased so much. I run now 10 miles 3 times a week and recommend Detox-Fit to all my friends and
+                              family !</p>
+                              <small>Jenn, East london</small>
+                            </div>
+                         </div>
+                       </blockquote>
+                     </div>
+                        <!-- Quote 2 -->
+                      <div class="item active left">
+                         <blockquote>
+                           <div class="row">
+                             <div class="col-sm-8 col-sm-offset-2">
+                                <p>When I was shopping around for a personal trainer I really wanted to have someone very reliable, to achieve my results and to get nutritional support. itonefitness really fulfilled all of these expectations and I must say I was very impressed with their diet help, support and tips" moe "Fantastic service, outstanding trainers, very
+                                professional and real experts in their field…. What more can I say…. Well, I''ve lost 3 jean sizes in no time and got rid of my bingo wings finally. I've already recommended Itonefitness my sister. </p>
+                                <small> Rebecca, Barnet</small>
+                             </div>
+                           </div>
+                         </blockquote>
+                     </div>
+                          <!-- Quote 3 -->
+                     <div class="item next left">
+                        <blockquote>
+                          <div class="row">
+                            <div class="col-sm-8 col-sm-offset-2">
+                              <p>"I've lost 7 inches of my waist and 3 inches of my thighs, and this is just the beginning. Thanks to moe  </p>
+                              <small>Amanda, Chelsea</small>
+                            </div>
+                          </div>
+                        </blockquote>
+                     </div>
+               </div>
+                    <!-- Carousel Buttons Next/Prev -->
+                      <a data-slide="prev" href="#quote-carousel" class="left carousel-control"><i class="fa fa-chevron-left"></i></a>
+                      <a data-slide="next" href="#quote-carousel" class="right carousel-control"><i class="fa fa-chevron-right"></i></a>
+         </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div id="about">
+   <div class="container">
+      <div class="row">
+          <div class="col-md-6 col-sm-6">
+              <h1>About Itone Fitness</h1>
+                <p class="wht">itonefitness was founded in 2012 and rapidly became
+                  one of most dynamic, fastest growing and innovative companies in the
+                  fitness industry taking London literally by storm. With a team of over
+                  13 personal trainers which continues to expand quickly and many hundreds
+                   of very happy customers, Itonefitnesss stood out by itself by showing
+                  an outstanding professionalism, care and dedication to its personal
+                  training customers and, above all, by delivering outstanding results.
+                  Behind itonefitness success lies the remarkable and life changing true
+                  story of its founder Mounder Sotehi . He started personal training and
+                  fitness world and introduced the itonefitness, the infallible formula to
+                   achieving amazing personal training results, we believe no result
+                  refund fee back </p>
+          </div>
+        <div class="col-md-6 col-sm-6"></div>
+     </div>
+   </div>
+</div>
+
+<div id="fitness">
+  <div class="container">
+     <div class="row">
+         <div class="col-md-12">
+
+         </div>
+     </div>
+  </div>
+</div>
+
+<div class="medium-contant">
+    <div class="container">
+      <div class="row">
+          <div class="col-md-12">
+            <h1 class="medium-contant-h1">Suck it up and some day you wont have to suck it in</h1>
+          </div>
+      </div>
+    </div>
+</div>
+
+<footer>
+   <div class="container">
+      <div class="row">
+          <div class="col-md-12 text-center">
+            <h1>ITONE FITNESS</h1>
+             <p class="wht">North &amp; Central London<br>
+             <i class="fa fa-phone"></i> 0751 9234 207<br>
+             <a class="wht" href="mailto:itonefitnessmoe@gmail.com"> <i class="fa fa-envelope"></i> itonefitnessmoe@gmail.com </a></p>
+             <p class="text-muted">© 2015 Itone Fitness. All Rights Reserved.
+             </p>
+             <p>
+                <a href="http://jigsaw.w3.org/css-validator/check/referer">
+                  <img style="border:0;width:88px;height:31px margin:15px;" src="http://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS!" />
+                </a>
+                <a href="http://jigsaw.w3.org/css-validator/check/referer">
+                  <img style="border:0;width:88px;height:31px" src="http://jigsaw.w3.org/css-validator/images/vcss-blue" alt="Valid CSS!"/></a>
+               </p>
+
+          </div>
+      </div>
+   </div>
+</footer>
+</body>
+
+<script type="text/javascript" src="js/jquery.js"></script>
+        <script type="text/javascript" src="js/bootstrap.js"></script>
+</html>
